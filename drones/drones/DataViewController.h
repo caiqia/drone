a72e4e1e89110f6e26246761b3b7cf1ff56614c9
Qiaoshan Cai <cai@MacBook-Pro-de-Qiaoshan.local>
2018-02-25 19:57:35 +0100

    lucie: ajoute label dans dataview. essaie utiliser tableview avec prinpalTableView maisça ne fonctionne pas

--- drones/drones/DataViewController.h
+++ drones/drones/DataViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface DataViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *wifiLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bebopLabel;
+@property (weak, nonatomic) IBOutlet UILabel *liveLabel;
+@property (weak, nonatomic) IBOutlet UILabel *choegLabel;
 
 @property (strong, nonatomic) IBOutlet UILabel *dataLabel;
 @property (strong, nonatomic) id dataObject;
---
+++ drones/drones/PrincipalTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  PrincipalTableViewController.h
+//  drones
+//
+//  Created by Qiaoshan Cai on 25/02/2018.
+//  Copyright © 2018 MASRI CHOUKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrincipalTableViewController : UITableViewController
+@property (weak, readonly,nonatomic) IBOutlet UILabel *wifiLabel;
+
+@end
