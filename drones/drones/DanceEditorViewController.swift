9962e6da1b0a1902355625899505535b55af2142
CAI QIAOSHAN <3605744@ppti-14-508-M-02.ufr-info-p6.jussieu.fr>
2018-03-13 19:37:45 +0100

    qiaoshan:DanceEditorView error when insert outlet connection

--- drones/drones/DanceEditorViewController.swift
+++ drones/drones/DanceEditorViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class DanceEditorViewController: UIViewController {
+class DanceEditorViewController: UIViewController, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var sliderTimer: UISlider!
     var dataObject: String = ""
-
+    // qiaoshan :
+    var audioPlayer = AVAudioPlayer()
+    let mp = MPMusicPlayerController.systemMusicPlayer()
+    var timer = NSTimer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // qiaoshan : The audio file is extracted from the Application's bundle
+        mp.prepareToPlay()
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.timerFired(_:)), userInfo: nil, repeats: true)
+        self.timer.tolerance = 0.1
+        mp.beginGeneratingPlaybackNotifications()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(ViewController.updateNowPlayingInfo), name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification, object: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +41,11 @@ class DanceEditorViewController: UIViewController {
         super.viewWillAppear(animated)
         self.dataLabel!.text = dataObject
     }
-
+    //qioashan :
+    func updateNowPlayingInfo(){
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.timerFired(_:)), userInfo: nil, repeats: true)
+        self.timer.tolerance = 0.1
+    }
 
 }
 
