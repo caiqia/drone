35ef19fc3a515b6c24345b14537b050adf9f0854
Qiaoshan Cai <qiaoshan.cai@mystream.fr>
2018-04-26 01:14:36 +0200

    qiaoshan: compelet dance editor

---
+++ ddrones/Pods/Target Support Files/Pods-dronesUITests/Pods-dronesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dronesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_dronesUITests
+@end
---
+++ ddrones/Views/Dancelist.swift
@@ -0,0 +1,13 @@
+//
+//  Dancelist.swift
+//  drones
+//
+//  Created by Qiaoshan Cai on 25/04/2018.
+//  Copyright © 2018 PSAR. All rights reserved.
+//
+
+import UIKit
+
+class Dancelist: NSObject {
+    
+}
